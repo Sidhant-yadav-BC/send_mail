5b0522c3ca160b973c5bb8beb64cc0f1688c6738
Sidhant-yadav-BC <sidhant.yadav@blenheimchalcot.com>
2023-10-11 17:19:41 +0530

    made changes

--- main.py
+++ main.py
@@ -1,41 +1,37 @@
+from flask import Flask, render_template, redirect, url_for
 import os
 import smtplib
+import imghdr
 from email.message import EmailMessage
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.mime.application import MIMEApplication
 from dotenv import load_dotenv
-
 load_dotenv()
 
-EMAIL_ADDRESS = "yrishu71@gmail.com"
+app = Flask(__name__)
+
+EMAIL_ADDRESS = os.environ.get('EMAIL_USER')
 EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
 
 contacts = ['YourAddress@gmail.com', 'test@example.com']
 
-msg = MIMEMultipart()
+msg = EmailMessage()
 msg['Subject'] = 'Check out Bronx as a puppy!'
 msg['From'] = EMAIL_ADDRESS
 msg['To'] = 'sidhantyadav92@gmail.com'
 
-msg.attach(MIMEText('This is a plain text email', 'plain'))
+msg.set_content('This is a plain text with falsk')
 
-msg.attach(MIMEText("""\
-<!DOCTYPE html>
-<html>
-    <body>
-        <h1 style="color:SlateGray;">This is an HTML Email!</h1>
-    </body>
-</html>
-""", 'html'))
-
-# Attach PDF file
-pdf_filename = './file.pdf'
-with open(pdf_filename, 'rb') as pdf_file:
-    pdf_attach = MIMEApplication(pdf_file.read(), _subtype="pdf")
-    pdf_attach.add_header('Content-Disposition', f'attachment; filename={os.path.basename(pdf_filename)}')
-    msg.attach(pdf_attach)
-
-with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-    smtp.send_message(msg)
+    
+@app.route("/")
+def home():
+    return render_template('home.html')
+    
+@app.route("/send", methods=["POST", "GET"])
+def send():
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        smtp.send_message(msg)
+    return redirect(url_for("home"))
+    
+    
+if __name__ == '__main__' :
+   app.run(debug=False, port="0.0.0.0")
